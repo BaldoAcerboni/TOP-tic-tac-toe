370538aa6410dfb80a2039ce167fd595caf37dce
Baldo Acerboni <baldo.acerboni@gmail.com>
2024-01-12 18:18:13 +0100

    Done AI logic for when user is player 2
    TO DO:
    make logic for AI when user starts game
    all HTML and CSS for pvp/pve setting
    try messing about and find bugs
    delete all console.log() calls
    
    NEW FEATURE TO ADD:
    add difficulty setting

--- script.js
+++ script.js
@@ -71,6 +71,7 @@ const game = (function () {
 
   const isGameOver = function () {
     const board = Gameboard.getGameboard();
+    console.log("first");
 
     if (board[1][1]) {
       if (
@@ -79,7 +80,6 @@ const game = (function () {
       ) {
         gameOver = true;
         _winner = board[1][1];
-        displayController.gameOverDisplay();
       }
     }
     for (let i = 0; i < 3; i++) {
@@ -90,7 +90,6 @@ const game = (function () {
       ) {
         gameOver = true;
         _winner = board[i][0];
-        displayController.gameOverDisplay();
       } else if (
         board[0][i] &&
         board[0][i] === board[1][i] &&
@@ -98,17 +97,17 @@ const game = (function () {
       ) {
         gameOver = true;
         _winner = board[0][i];
-        displayController.gameOverDisplay();
       }
     }
     if (_winner === 0 && _roundCount === 9) {
       gameOver = true;
-      displayController.gameOverDisplay();
     }
     if (gameOver) {
+      displayController.gameOverDisplay();
       Array.from(cells).forEach((cell) => {
-        cell.removeEventListener("click", displayController.userMovePvp);
+        cell.removeEventListener("click", displayController.playerMove);
       });
+      console.log("last");
     }
   };
 
@@ -141,38 +140,118 @@ const game = (function () {
 const AILogic = (function () {
   const AIPlayer = Players.getP1(); //TEMP
   const userPlayer = Players.getP2(); //TEMP
+  const board = Gameboard.getGameboard();
 
-  //create a copy without reference of multi-dimensional array, it might not create problems with all the protections
-  //that factory functions guarantee but why risk it? Hopefully performance won't be an issue.
-  //i don't even know if it is really necessary but i think i need it for the AI logic
-  const createBoardCopy = function () {
-    let boardCopy = [];
+  const winningMove = function (playerId) {
+    let win = false;
     for (let i = 0; i < 3; i++) {
-      boardCopy[i] = board[i].slice(0);
+      for (let j = 0; j < 3; j++) {
+        const boardCopy = [];
+        for (let k = 0; k < 3; k++) {
+          boardCopy[k] = board[k].slice(0);
+        }
+        if (boardCopy[i][j] === 0) {
+          boardCopy[i][j] = playerId;
+
+          //the crap below is basically a repetition of part of game.isGameOver function
+          //need to optimize when i got it working properly
+          if (boardCopy[1][1] === playerId) {
+            if (
+              (boardCopy[0][0] === boardCopy[1][1] &&
+                boardCopy[1][1] === boardCopy[2][2]) ||
+              (boardCopy[0][2] === boardCopy[1][1] &&
+                boardCopy[1][1] === boardCopy[2][0])
+            ) {
+              win = true;
+            }
+          }
+          for (let i = 0; i < 3; i++) {
+            if (
+              boardCopy[i][0] === playerId &&
+              boardCopy[i][0] === boardCopy[i][1] &&
+              boardCopy[i][0] === boardCopy[i][2]
+            ) {
+              win = true;
+            } else if (
+              boardCopy[0][i] === playerId &&
+              boardCopy[0][i] === boardCopy[1][i] &&
+              boardCopy[0][i] === boardCopy[2][i]
+            ) {
+              win = true;
+            }
+          }
+        }
+        if (win) {
+          console.log([i, j]);
+          return [i, j];
+        }
+      }
     }
-    return boardCopy;
+    return false;
   };
 
   //need to put AI selector inside displayController
   const move = (/* difficulty(?) */) => {
-    const board = Gameboard.getGameboard();
     const round = game.getRoundNr();
+    const userWin = winningMove(userPlayer.id);
+    const AIWin = winningMove(AIPlayer.id);
+    const random = randomMove();
 
-    if (!round) {
-      Gameboard.updateGameboard(AIPlayer, 0, 0);
-      msg.textContent = "Player's turn";
+    if (round === 0) {
+      Gameboard.updateGameboard(AIPlayer.id, 0, 0);
       document.getElementById(`0-0`).textContent = AIPlayer.symbol;
-
-      Array.from(cells).forEach((cell) => {
-        cell.addEventListener("click", displayController.userMovePvp);
-      });
     } else if (round === 2) {
-      if (board[0][2] === 0) {
-        Gameboard.updateGameboard(AIPlayer, 0, 0);
-        msg.textContent = "Player's turn";
+      if (board[0][2] === 0 && board[0][1] === 0) {
+        Gameboard.updateGameboard(AIPlayer.id, 0, 2);
         document.getElementById(`0-2`).textContent = AIPlayer.symbol;
+      } else {
+        Gameboard.updateGameboard(AIPlayer.id, 2, 0);
+        document.getElementById(`2-0`).textContent = AIPlayer.symbol;
+      }
+    } else if (round === 4) {
+      if (AIWin) {
+        Gameboard.updateGameboard(AIPlayer.id, AIWin[0], AIWin[1]);
+        document.getElementById(`${AIWin[0]}-${AIWin[1]}`).textContent =
+          AIPlayer.symbol;
+      } else if (userWin) {
+        Gameboard.updateGameboard(AIPlayer.id, userWin[0], userWin[1]);
+        document.getElementById(`${userWin[0]}-${userWin[1]}`).textContent =
+          AIPlayer.symbol;
+      } else if (board[2][2] === 0) {
+        Gameboard.updateGameboard(AIPlayer.id, 2, 2);
+        document.getElementById(`2-2`).textContent = AIPlayer.symbol;
+      } else {
+        Gameboard.updateGameboard(AIPlayer.id, 2, 0);
+        document.getElementById(`2-0`).textContent = AIPlayer.symbol;
+      }
+    } else if (round > 4) {
+      if (AIWin) {
+        Gameboard.updateGameboard(AIPlayer.id, AIWin[0], AIWin[1]);
+        document.getElementById(`${AIWin[0]}-${AIWin[1]}`).textContent =
+          AIPlayer.symbol;
+      } else if (userWin) {
+        Gameboard.updateGameboard(AIPlayer.id, userWin[0], userWin[1]);
+        document.getElementById(`${userWin[0]}-${userWin[1]}`).textContent =
+          AIPlayer.symbol;
+      } else {
+        Gameboard.updateGameboard(AIPlayer.id, random[0], random[1]);
+        document.getElementById(`${random[0]}-${random[1]}`).textContent =
+          AIPlayer.symbol;
       }
     }
+
+    displayController.playerMoveAddEventListener();
+  };
+  const randomMove = function () {
+    const possibleMoves = [];
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === 0) {
+          possibleMoves.push([i, j]);
+        }
+      }
+    }
+    return possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
   };
   return { move };
 })();
@@ -183,49 +262,46 @@ const displayController = (function () {
 
   const msg = document.getElementById("msg");
 
-  //move based on two human players need pvp option
-  const whichUser = () => {
+  const whichPlayer = () => {
     if (p1.turn) {
-      const user = p1;
-      return user;
+      const player = p1;
+      return player;
     } else {
-      const user = p2;
-      return user;
+      const player = p2;
+      return player;
     }
   };
 
-  const userMovePvp = function (e) {
+  const playerMoveAddEventListener = function () {
+    Array.from(cells).forEach((cell) => {
+      cell.addEventListener("click", playerMove);
+    });
+  };
+  const playerMoveRemoveEventListener = function () {
+    Array.from(cells).forEach((cell) => {
+      cell.removeEventListener("click", playerMove);
+    });
+  };
+
+  const playerMove = function (e) {
     const pos = e.target.id.split("-");
     const posX = Number(pos[0]);
     const posY = Number(pos[1]);
     const boardSituation = Gameboard.getGameboard();
 
     if (boardSituation[posX][posY] === 0) {
-      e.target.textContent = whichUser().symbol;
+      e.target.textContent = whichPlayer().symbol;
       msg.textContent = "";
-      Gameboard.updateGameboard(whichUser().id, posX, posY);
+      Gameboard.updateGameboard(whichPlayer().id, posX, posY);
     } else {
       msg.textContent = `Position has already been selected by player${boardSituation[posX][posY]}`;
+      return;
     }
-  };
-
-  //DRY MIA(when i got something working i'll fix it)
-  const userMovePve = function (e) {
-    const pos = e.target.id.split("-");
-    const posX = Number(pos[0]);
-    const posY = Number(pos[1]);
-    const boardSituation = Gameboard.getGameboard();
-
-    if (boardSituation[posX][posY] === 0) {
-      e.target.textContent = whichUser().symbol;
-      msg.textContent = "";
-      Gameboard.updateGameboard(whichUser().id, posX, posY);
-
-      Array.from(cells).forEach((cell) => {
-        cell.removeEventListener("click");
-      });
-    } else {
-      msg.textContent = `Position has already been selected by player${boardSituation[posX][posY]}`;
+    if (p1.userStatus === "AI" || p2.userStatus === "AI") {
+      if (whichPlayer().userStatus === "AI") {
+        playerMoveRemoveEventListener();
+        AILogic.move();
+      }
     }
   };
 
@@ -241,24 +317,31 @@ const displayController = (function () {
     game.resetGameOver();
 
     Array.from(cells).forEach((cell) => {
-      cell.addEventListener("click", displayController.userMovePvp);
+      cell.addEventListener("click", displayController.playerMove);
     });
   };
 
   const gameOverDisplay = function () {
     const winner = game.getWinner();
+    console.log(winner);
     if (winner) {
+      console.log(`The winner is player${winner}`);
       msg.textContent = `The winner is player${winner}`;
     } else {
       msg.textContent = `The game ends in a draw`;
     }
   };
 
-  return { userMovePvp, clearDisplay, gameOverDisplay };
+  return {
+    playerMove,
+    clearDisplay,
+    gameOverDisplay,
+    playerMoveAddEventListener,
+  };
 })();
 
 /* Array.from(cells).forEach((cell) => {
-  cell.addEventListener("click", displayController.userMovePvp);
+  cell.addEventListener("click", displayController.playerMove);
 }); */
 
 AILogic.move();
@@ -268,8 +351,10 @@ clearBtn.addEventListener("click", displayController.clearDisplay);
 /*
 TO DO LIST:
 FIX
-smash your head against the ai and find a somewhat doable solution
+make logic for AI when user starts game
+all HTML and CSS for pvp/pve setting
 try messing about and find bugs
+delete all console.log() calls
 NEW FEATURE:
-need to build AI player from scratch, with difficulty settings(?);
+difficulty settings;
  */
